a16b9ca6b7a918f8986bf62fbed2dec0f95555ca
Oliver Evans <olly.evans@hogarth.com>
2024-12-10 13:43:04 +0000

    cleaned up code and added requirements file

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask, render_template, request
-from rovergpt import get_rover_img_url, get_dalle_image, gpt_describe #get_dalle_filename, get_rover_filename,
+from rovergpt import get_rover_img_url, get_dalle_image, gpt_describe
 
 app = Flask(__name__, static_url_path = '/static')
 
 default_date = 'yyyy-mm-dd'
 default_rover_image = '/static/Default_Rover_Image.png'
 default_dalle_image = '/static/Default_Dalle_Image.png'
+
 @app.route('/')
 def index():
 	return render_template("index.html", date=default_date, rover_img_filepath=default_rover_image, dalle_img_filepath=default_dalle_image) # dalle_img_filepath=dalle_file, rover_img_filepath=rover_file)
 
 @app.route('/', methods=['POST'])
 def index_post():
-# TO DO!!! work out this below, and how to make it interactive, see index
 	user_date = request.form['req_date']
 	rover_img_url = get_rover_img_url(user_date)
 	dalle_img = get_dalle_image(rover_img_url)
 	gpt_prompt = gpt_describe(rover_img_url)
-	# user_prompt = request.form['req_prompt']
-	# dalle_input = get_dalle_filename() # TO DO - i think these variables will go in these functions
-	# rover_input = get_rover_filename()
-	print(rover_img_url)
-	print(dalle_img)
 	return render_template('index.html', rover_img_filepath=rover_img_url, dalle_img_filepath=dalle_img, gpt_output=gpt_prompt)
--- rovergpt.py
+++ rovergpt.py
@@ -9,46 +9,19 @@ with open('nasakey.txt', 'r') as nasa_file:
 	nasa_key = nasa_file.read().strip()
 
 
-def get_rover_img_url(rover_date): # DATE IN FUNCTION # TO DO - check this ufnciton for getting the URL of the image, to then feed into gpt
-	# TO DO - how can this varible become interactive?
-	# TO DO work out how to have the user input a date to update the rover pics on the website
+def get_rover_img_url(rover_date): 
 	url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={rover_date}&api_key={nasa_key}"
-	print(rover_date)
 	response = requests.get(url)
 	data = response.json()
-	# print(data)
 	rover_img_url = data["photos"][0]["img_src"]
-	print (rover_img_url)
 	return rover_img_url
 
 def get_dalle_image(rover_img_url):
 	bot_response = gpt_describe(rover_img_url)
-	print (f"this is the bot response {bot_response}")
-	print("end of bot reponse")
-	# THE ISSUE IS BOT_RESPONSE
 	new_dalle_img = make_dalle_img(bot_response)
 	return new_dalle_img
 
-# rover_folder = 'static'
-# rover_img_dir = os.path.join(os.curdir, rover_folder)
-
-# if not os.path.isdir(rover_img_dir):  
-#     os.mkdir(rover_img_dir)
-
-# # Would like to save every image using a timestamp
-# rover_filename = 'Rover_Image.png'
-# rover_img_path = os.path.join(rover_img_dir, rover_filename)
-
-# # rover_gen_image = requests.get(rover_img).content
-
-# with open(rover_img_path, 'wb') as file:  
-#     file.write(rover_gen_image)
-
-# def get_rover_filename():
-#     return f"/{rover_folder}/{rover_filename}"  
-
 # DALLE
-
 client = AzureOpenAI(
 	api_key = os.getenv("AZURE_KEY"),
 	api_version = "2024-10-21",
@@ -56,69 +29,37 @@ client = AzureOpenAI(
 )
 
 dalle_folder= 'static'
-# setting up a file path inside the current folder called images, where the dalle repsonses will be stored.
 dalle_image_dir = os.path.join(os.curdir, dalle_folder)
 
-# if this folder doesn't already exist, make a new one
 if not os.path.isdir(dalle_image_dir):
 	os.mkdir(dalle_image_dir)
 
 def make_dalle_img(bot_response): 
-	# generate an image using Dalle
-	# my dalle prompt will be the GPT output
 	dalle_prompt = bot_response
-	print (f"this is the dalle prompt {dalle_prompt}")
 
 	dalle_result = client.images.generate(
 		model = "dalle3", 
 		prompt = dalle_prompt,
 		n=1
 		)
-	# convert the dalle repsone to json
-	json_response = json.loads(dalle_result.model_dump_json())
-	# print(dalle_result)
 
-	# creating my file path 
-	# the path of the folder + the file name
+	json_response = json.loads(dalle_result.model_dump_json())
+
 	dalle_filename = 'Dalle_Image.png'
 	image_path = os.path.join(dalle_image_dir, dalle_filename)
-	# get the url that openai gives us
 	image_url = json_response['data'][0]['url']
-	# revised_prompt = json_response['data'][0]['revised_prompt']
 
-	# use the url to give me the actual image
 	dalle_gen_image = requests.get(image_url).content
 
-	# save the image in my images folder
+
 	with open(image_path, 'wb') as file:
 		file.write(dalle_gen_image)
 
 	return '/static/'+dalle_filename
 
-# this will have python open the image file - good for debugging and proof of concept
-# image = Image.open(image_path)
-# image.show()
-
 def get_dalle_filename():
 	return f"/{dalle_folder}/{dalle_filename}"
 
-# makes the alt text the revised prompt
-def get_prompt():
-	return revised_prompt
-
-
-# CHATGPT
-# def gpt_describe(rover_img):
-# 	messages = [
-# 		{"role": "user", "content": [
-# 		{"type": "text", "text": "Analyze the provided image captured by the Mars Rover. In no more than 1500 characters, describe the terrain, colors, textures, and notable features in detail. Then, based on the environmental conditions depicted in the image, write a short paragraph of speculative fiction about a tool or piece of equipment necessary for human survival on Mars. Include its purpose and how it is designed to adapt to the specific challenges shown in the image."},
-# 		{"type": "image_url", 
-# 		# "image_url": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01775/opgs/edr/fcam/FLB_555078469EDR_F0642790FHAZ00341M_.JPG"}
-# 		"image_url": f"{rover_img}"
-# 		}
-# 		]
-# 	}
-# 	]
 
 def gpt_describe(rover_img):
 	messages = [
@@ -140,24 +81,10 @@ def gpt_describe(rover_img):
 		}
 	]
 
-
-
-# def gpt_describe(rover_img):
-# 	messages = [
-# 		{
-# 			"role": "user",
-# 			"content": (
-# 				f"{rover_img}"
-
-# 			),
-# 		}
-# 	]
-
 	response = client.chat.completions.create(
 		model="gpt-4o",
 		messages=messages,
 		tools=functions,
-		# auto means ChatGPT decides when to use external functions
 		tool_choice="auto"
 	)
 	return response.choices[0].message.content
@@ -170,7 +97,6 @@ functions = [
 			"name": "get_img_url",
 			"description": "Gets the image url from the Mars Rover Api",
 			"parameters": {
-					# letting chatgpt know that it's geting key-value pairs
 				"type": "object",
 				"properties": {
 					"rover_date": {
@@ -185,35 +111,3 @@ functions = [
 		}
 	}
 ]
-
-# # print(response.choices[0].message)
-# response_message = response.choices[0].message 
-# gpt_tools = response.choices[0].message.tool_calls
-
-# if gpt_tools:
-# 	available_functions = {
-# 			"get_img_url": get_rover_img_url
-# 	}
-
-# 	messages.append(response_message)
-# 	for gpt_tool in gpt_tools:
-# 		function_name = gpt_tool.function.name
-# 		function_to_call = available_functions[function_name]
-# 		function_parameters = json.loads(gpt_tool.function.arguments)
-# 		function_response = function_to_call(function_parameters.get('rover_date'))
-# 		messages.append(
-# 			{
-# 				"tool_call_id": gpt_tool.id,
-# 				"role": "tool",
-# 				"name": function_name,
-# 				"content": function_response
-# 			}
-# 		)
-# 		second_response = client.chat.completions.create(
-# 			model = "gpt-4o",
-# 			messages=messages
-# 		)
-# 		print(second_response.choices[0].message.content)
-
-# else:
-# 	print(response.choices[0].message.content)
